fbfc978cb950d0e77b504c90ec73d1f71bbf9183
ankie2020 <ankereinelt@gmx.de>
2021-04-06 18:03:20 +0200

    Initial Commit

---
+++ Hello Swift/Hello_SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_SwiftApp.swift
+//  Hello Swift
+//
+//  Created by Anke  on 06.04.21.
+//
+
+import SwiftUI
+
+@main
+struct Hello_SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
